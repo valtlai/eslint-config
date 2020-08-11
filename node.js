191f8e271ba31dc51d263454c84100cc0d2b6130
Valtteri Laitinen <dev@valtlai.fi>
2020-08-11 05:10:06 +0300

    Remove `gulpfile.mjs` override

--- node.js
+++ node.js
@@ -5,11 +5,5 @@ const rules = require('./rules/node');
 module.exports = {
 	env: { node: true },
 	extends: 'plugin:node/recommended',
-	rules,
-	overrides: [
-		{
-			files: 'gulpfile.mjs',
-			rules: { 'require-await': 'off' }
-		}
-	]
+	rules
 };
