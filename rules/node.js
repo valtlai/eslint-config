5e70d454b8aaff9ad172a389332191b75e4074e8
Valtteri Laitinen <dev@valtlai.fi>
2020-09-21 23:59:38 +0300

    Disable `no-unpublished-import` on private module

--- rules/node.js
+++ rules/node.js
@@ -1,13 +1,14 @@
 'use strict';
 
+const getPackageJson = require('eslint-plugin-node/lib/util/get-package-json');
+const isPrivate = getPackageJson()?.private === true;
+
 module.exports = {
 	/** plugin:node **/
 	'no-process-exit': 'off', // ~> unicorn/no-process-exit // @override
 	// Possible Errors
 	'node/handle-callback-err': ['error', '^err(?:or)?$'],
 	'node/no-path-concat': 'error',
-	'node/no-unpublished-import': ['error', { allowModules: ['ava'] }], // @override
-	'node/no-unpublished-require': ['error', { allowModules: ['ava'] }], // @override
 	// Stylistic Issues
 	'node/file-extension-in-import': 'error',
 	'node/global-require': 'error',
@@ -26,3 +27,8 @@ module.exports = {
 	'unicorn/no-new-buffer': 'error', // to autofix (~> node/no-deprecated-api)
 	'unicorn/no-process-exit': 'error',
 };
+
+if (isPrivate) {
+	module.exports['node/no-unpublished-import'] = 'off'; // @override
+	module.exports['node/no-unpublished-require'] = 'off'; // @override
+}
