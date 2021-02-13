e5cd7adc2c0bb41d5f9508a237270cd889d4bc3c
Valtteri Laitinen <dev@valtlai.fi>
2021-02-13 02:52:38 +0200

    Restore back-compat to lockfile

--- .lockfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs/promises');
-const json = require('./package-lock.json');
-
-delete json.dependencies;
-
-fs.writeFile('package-lock.json', JSON.stringify(json, null, '\t') + '\n');
